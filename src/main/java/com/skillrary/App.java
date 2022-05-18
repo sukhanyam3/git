9e2a533783abac8af6ccb1740126df879151d5ff
sukhanya <sukhanyam3@gmail.com>
2022-05-18 14:46:28 +0530

    n1

---
+++ src/main/java/com/skillrary/App.java
@@ -0,0 +1,13 @@
+package com.skillrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
